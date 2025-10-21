694cbdf0aee2c4916c1ce17a0dcdf84ca7554957
Preet Kaler <preetkalerop@gmail.com>
2025-10-21 17:14:19 +0530

    new

---
+++ Leet2434.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Leet2434 {
+    public String robotWithString(String s) {
+        int n = s.length();
+        int[] freq = new int[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        Stack<Character> st = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        char minChar = 'a';
+
+        for (char c : s.toCharArray()) {
+            st.push(c);
+            freq[c - 'a']--;
+
+            while (minChar <= 'z' && freq[minChar - 'a'] == 0)
+                minChar++;
+
+            while (!st.isEmpty() && st.peek() <= minChar)
+                res.append(st.pop());
+        }
+
+        while (!st.isEmpty())
+            res.append(st.pop());
+
+        return res.toString();
+    }
+}
