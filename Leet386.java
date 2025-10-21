2612b294e3aca9890a739fbbfb807379777fed3f
Preet Kaler <preetkalerop@gmail.com>
2025-10-21 17:17:06 +0530

    new

---
+++ Leet386.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leet386 {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans = new ArrayList<>();
+        int count = 1;
+
+        for(int i = 0; i < n; i++){
+            ans.add(count);
+            if(count * 10 <= n){
+                count *= 10;
+            }else{
+                if(count >= n)count /= 10;
+                count++;
+                while(count % 10 == 0){
+                    count /= 10;
+                }
+            }
+        }
+        return ans;
+    }
+}
